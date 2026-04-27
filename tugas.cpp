7724348d890eb75d8a4b2d7104b7399c7d03ccd7
Bendadiotulada-323 <bendadiotulada@gmail.com>
2026-04-27 18:55:46 +0700

    add class layang layang and make variable in private and public

---
+++ tugas.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <cmath>
+using namespace std;
+
+class BelahKetupat;
+class LayangLayang
+{
+    private:
+        float d1, d2;
+        float sisi1, sisi2;
+    public:
+        void input();
+        float Luas();
+        float Keliling();
+        void output();
+        
+        // Friend function declaration
+        friend class BelahKetupat;
+};
+
